b133f45dd8be32230a2ddb99740c9565ea863400
candra miftah <candragain99@gmail.com>
2022-08-19 15:59:29 +0700

    composition api

--- resources/ts/src/views/account/forms/DataDiri.vue
+++ resources/ts/src/views/account/forms/DataDiri.vue
@@ -1,9 +1,10 @@
 <template>
-    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="200px" class="demo-ruleForm">
+    <el-form   :rules="rules" ref="ruleForm" label-width="200px" class="demo-ruleForm">
       <el-form-item label="Nama Lengkap" prop="name">
           <el-input v-model="ruleForm.name"></el-input>
       </el-form-item>
-      <el-form-item prop="email" label="Email" >
+    </el-form>
+      <!-- <el-form-item prop="email" label="Email" >
           <el-input v-model="ruleForm.email"></el-input>
       </el-form-item>
       <el-form-item label="Gelar">
@@ -57,8 +58,15 @@
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="Alamat" prop="alamat">
-        <el-input type="textarea" v-model="ruleForm.alamat"></el-input>
+      <el-form-item label="Alamat Asal" prop="alamat">
+        <el-input type="textarea" v-model="ruleForm.alamat" @input="searchRegion()"></el-input>
+        <el-autocomplete
+            class="inline-input w-100"
+            v-model="ruleForm.alamat"
+            :fetch-suggestions="querySearch"
+            :trigger-on-focus="false"
+            @select="handleSelect">
+        </el-autocomplete>
       </el-form-item>
       <el-form-item label="Nomor Ponsel" prop="nomorHP">
         <el-input v-model="ruleForm.nomorHP"></el-input>
@@ -84,131 +92,223 @@
         </el-select>
       </el-form-item>
       <el-form-item>
-          <el-button @click="resetForm('ruleForm')">Reset</el-button>
+          <el-button @click="resetForm('ruleForm')">Reset</el-button> -->
           <el-button type="primary" @click="submitForm('ruleForm')">Next</el-button>
-      </el-form-item>
-    </el-form>
+      <!-- </el-form-item>
+    </el-form> -->
 
 </template>
-<script>
-    import axios from 'axios'
-  export default {
-    data() {
-        return {
-            options: {
-              'etnicity': [],
-              'marital': [],
-              'religion': [],
-            },
-            ruleForm: {
-                name: '',
-                email: '',
-                etnicity: '',
-                marital: '',
-                religion: '',
-                gelarAwal: '',
-                gelarAkhir: '',
-                nomorKTP: '',
-                nomorNPWP: '',
-                nomorSIM: '',
-                tempatLahir: '',
-                tanggalLahir: '',
-                golonganDarah: '',
-                alamat: '',
-                nomorHP: '',
-            },
-            rules: {
-              name: [
-                { required: true, message: 'Mohon isi nama lengkap', trigger: 'blur' },
-                { min: 3, message: 'Panjang karakter harus lebih dari 3', trigger: 'blur' }
-              ],
-              email: [
-                { required: true, message: 'Mohon isi email', trigger: 'blur' },
-                { type: 'email', message: 'Mohon isi format email yang benar', trigger: ['blur', 'change'] }
-              ],
-              nomorKTP: [
-                { required: true, message: 'Mohon isi nomor KTP', trigger: ['blur', 'change'] },
-                { min: 16, message: 'Nomor KTP harus 16 digit', trigger: ['blur', 'change'] },
-                { max: 16, message: 'Nomor KTP harus 16 digit', trigger: ['blur', 'change'] }
-              ],
-              nomorNPWP: [
-                { min: 16, message: 'Nomor NPWP harus 16 digit', trigger: ['blur', 'change'] },
-                { max: 16, message: 'Nomor NPWP harus 16 digit', trigger: ['blur', 'change'] }
-              ],
-              nomorSIM: [
-                { min: 12, message: 'Nomor SIM harus 12 digit', trigger: ['blur', 'change'] },
-                { min: 12, message: 'Nomor SIM harus 12 digit', trigger: ['blur', 'change'] }
-              ],
-              tempatLahir: [
-                { required: true, message: 'Mohon isi Tempat Lahir', trigger: 'blur' },
-              ],
-              tanggalLahir: [
-                { required: true, message: 'Mohon isi Tanggal Lahir', trigger: 'blur' },
-              ],
-              alamat: [
-                { required: true, message: 'Mohon isi Alamat Lengkap', trigger: 'blur' },
-              ],
-              nomorHP: [
-                { required: true, message: 'Mohon isi nomor HP', trigger: 'blur' },
-              ],
-              marital: [
-                { required: true, message: 'Mohon isi pilih Status Pernikahan', trigger: ['blur', 'change'] },
-              ],
-              etnicity: [
-                { required: false, message: 'Mohon isi pilih Etnis', trigger: ['blur', 'change'] },
-              ],
-              religion: [
-                { required: true, message: 'Mohon isi pilih Agama', trigger: ['blur', 'change'] },
-              ],
-            }
-      };
-    },
-    mounted : function() {
-      this.getStatus()
-    },
-    methods: {
-      async getStatus(){
-        var vuex = JSON.parse(localStorage.getItem('vuex'))
-        var token = vuex.AuthModule.token
+<script lang="ts">
+  import axios from 'axios'
+  import { defineComponent, ref, onMounted, reactive } from 'vue'
+  import { useStore } from 'vuex';
 
-        await axios.get('/api/status', {
-                        headers: {'Authorization': 'Bearer '+ token},
-                    })
-          .then((response)=> {
-            response.data.forEach(element => {
-                if(element.status_id.slice(0,3) == 'ETH') {
-                    this.options['etnicity'].push({
-                        'value': element.status_id,
-                        'label': element.name,
-                    })
-                } else if(element.status_id.slice(0,3) == 'REL') {
-                    this.options['religion'].push({
-                        'value': element.status_id,
-                        'label': element.name,
-                    })
-                }
-                if(element.status_id.slice(0,3) == 'MAR') {
-                    this.options['marital'].push({
-                        'value': element.status_id,
-                        'label': element.name,
-                    })
-                }
-            });
-          })
-      },
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            this.nextStepAction()
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      }
+//   export default {
+//     data() {
+//         return {
+//             token: '',
+//             options: {
+//               'etnicity': [],
+//               'marital': [],
+//               'religion': [],
+//             },
+//             ruleForm: {
+//                 name: '',
+//                 email: '',
+//                 etnicity: '',
+//                 marital: '',
+//                 religion: '',
+//                 gelarAwal: '',
+//                 gelarAkhir: '',
+//                 nomorKTP: '',
+//                 nomorNPWP: '',
+//                 nomorSIM: '',
+//                 tempatLahir: '',
+//                 tanggalLahir: '',
+//                 golonganDarah: '',
+//                 alamat: '',
+//                 nomorHP: '',
+//             },
+//             rules: {
+//               name: [
+//                 { required: true, message: 'Mohon isi nama lengkap', trigger: 'blur' },
+//                 { min: 3, message: 'Panjang karakter harus lebih dari 3', trigger: 'blur' }
+//               ],
+//               email: [
+//                 { required: true, message: 'Mohon isi email', trigger: 'blur' },
+//                 { type: 'email', message: 'Mohon isi format email yang benar', trigger: ['blur', 'change'] }
+//               ],
+//               nomorKTP: [
+//                 { required: true, message: 'Mohon isi nomor KTP', trigger: ['blur', 'change'] },
+//                 { min: 16, message: 'Nomor KTP harus 16 digit', trigger: ['blur', 'change'] },
+//                 { max: 16, message: 'Nomor KTP harus 16 digit', trigger: ['blur', 'change'] }
+//               ],
+//               nomorNPWP: [
+//                 { min: 16, message: 'Nomor NPWP harus 16 digit', trigger: ['blur', 'change'] },
+//                 { max: 16, message: 'Nomor NPWP harus 16 digit', trigger: ['blur', 'change'] }
+//               ],
+//               nomorSIM: [
+//                 { min: 12, message: 'Nomor SIM harus 12 digit', trigger: ['blur', 'change'] },
+//                 { min: 12, message: 'Nomor SIM harus 12 digit', trigger: ['blur', 'change'] }
+//               ],
+//               tempatLahir: [
+//                 { required: true, message: 'Mohon isi Tempat Lahir', trigger: 'blur' },
+//               ],
+//               tanggalLahir: [
+//                 { required: true, message: 'Mohon isi Tanggal Lahir', trigger: 'blur' },
+//               ],
+//               alamat: [
+//                 { required: true, message: 'Mohon isi Alamat Lengkap', trigger: 'blur' },
+//               ],
+//               nomorHP: [
+//                 { required: true, message: 'Mohon isi nomor HP', trigger: 'blur' },
+//               ],
+//               marital: [
+//                 { required: true, message: 'Mohon isi pilih Status Pernikahan', trigger: ['blur', 'change'] },
+//               ],
+//               etnicity: [
+//                 { required: false, message: 'Mohon isi pilih Etnis', trigger: ['blur', 'change'] },
+//               ],
+//               religion: [
+//                 { required: true, message: 'Mohon isi pilih Agama', trigger: ['blur', 'change'] },
+//               ],
+//             }
+//       };
+//     },
+//     mounted : function() {
+//         var vuex = JSON.parse(localStorage.getItem('vuex'))
+//         this.token = vuex.AuthModule.token
+
+//       this.getStatus()
+//     },
+//     methods: {
+//       async getStatus(){
+
+//         await axios.get('/api/status', {
+//             headers: {'Authorization': 'Bearer '+ this.token},
+//         })
+//           .then((response)=> {
+//             response.data.forEach(element => {
+//                 if(element.status_id.slice(0,3) == 'ETH') {
+//                     this.options['etnicity'].push({
+//                         'value': element.status_id,
+//                         'label': element.name,
+//                     })
+//                 } else if(element.status_id.slice(0,3) == 'REL') {
+//                     this.options['religion'].push({
+//                         'value': element.status_id,
+//                         'label': element.name,
+//                     })
+//                 }
+//                 if(element.status_id.slice(0,3) == 'MAR') {
+//                     this.options['marital'].push({
+//                         'value': element.status_id,
+//                         'label': element.name,
+//                     })
+//                 }
+//             });
+//           })
+//       },
+//       resetForm(formName) {
+//         this.$refs[formName].resetFields();
+//       },
+
+//     //   async searchRegion() {
+
+//     //     var payload = this.ruleForm.alamat
+//     //     if(payload.length >= 3) {
+//     //         await axios.get('/api/region/search/' + payload, {
+//     //             headers: {'Authorization': 'Bearer '+ this.token},
+//     //         })
+//     //         .then(response => {
+//     //             console.log(response)
+//     //         })
+//     //     }
+
+//     //   }
+//     },
+//     setup() {
+//         const store = useStore();
+//         const restaurants = ref([]);
+//         const token = store.getters.getToken
+//         var results = [];
+
+//         const querySearch = async (queryString, cb) => {
+//                 var payload = queryString
+//                     if(payload.length >= 3) {
+//                         await axios.get('/api/region/search/' + payload, {
+//                             headers: {'Authorization': 'Bearer '+ token},
+//                         })
+//                         .then(response => {
+//                             results = [];
+//                             response.data.forEach(element => {
+//                                 results.push(
+//                                     {
+//                                         "value": element.name,
+//                                         "region_id": element.region_id
+//                                     },
+//                                 )
+//                             });
+//                         })
+//                     }
+//                 // call callback function to return suggestions
+//                 cb(results);
+//             };
+
+//             const createFilter = (queryString) => {
+//                 return (restaurant) => {
+//                     return (
+//                         restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0
+//                     );
+//                 };
+//             };
+
+
+//             const handleSelect = (item) => {
+//                 console.log(item);
+//             };
+
+//         return {
+//             restaurants,
+//             querySearch,
+//             createFilter,
+//             handleSelect,
+//         };
+//     },
+
+//   }
+
+    export default {
+        setup() {
+            const store = useStore();
+            const ruleForm = reactive({
+                name : '',
+                email : '',
+                etnicity : '',
+                marital : '',
+                religion : '',
+                gelarAwal : '',
+                gelarAkhir : '',
+                nomorKTP : '',
+                nomorNPWP : '',
+                nomorSIM : '',
+                tempatLahir : '',
+                tanggalLahir : '',
+                golonganDarah : '',
+                alamat : '',
+                nomorHP : ''
+            })
+
+            const options = {
+                'etnicity': [],
+                'marital': [],
+                'religion': [],
+            }
+
+            return {
+                ruleForm
+            }
+        },
+
     }
-  }
 </script>
